7b371007f4189a3f357c5ec66cd12c1c75ee97b2
Yoga Raditya <144443155+yogarn@users.noreply.github.com>
2023-09-10 17:41:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import watermark
+import sign
 
 # create argument parser
 parser = argparse.ArgumentParser(description="basic pdf tools using PyMuPDF")
@@ -8,7 +9,8 @@ parser = argparse.ArgumentParser(description="basic pdf tools using PyMuPDF")
 parser.add_argument("--mode", "-m", choices=["watermark", "sign", "extract"], help="programs mode, currently available : watermark", required=True)
 parser.add_argument("--input", "-i", help="input file path")
 parser.add_argument("--output", "-o", help="output file path")
-parser.add_argument("--text", "-t", help="used for watermark, extracting text (optional)")
+parser.add_argument("--text", "-t", help="used for watermark, extracting text, signature name")
+parser.add_argument("--signature", "-s", help="signature png file path")
 
 args = parser.parse_args()
 
@@ -16,8 +18,14 @@ args = parser.parse_args()
 if args.mode == "watermark":
     # require input, output, and text
     if args.input is None or args.output is None or args.text is None:
-        parser.error("Argumen '--input', '--output', dan '--text' required for watermark mode")
+        parser.error("Argument '--input', '--output', and '--text' required for watermark mode")
     else:
         watermark.start(args.input, args.output, args.text)
+elif args.mode == "sign":
+    # require input, output, signature, and text
+    if args.input is None or args.output is None or args.text is None:
+        parser.error("Argument '--input', '--output', '--signature', and '--text' required for sign mode")
+    else:
+        sign.start(args.input, args.output, args.signature, args.text)
 else:
     print(f"Mode: {args.mode}")
