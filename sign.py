1d34171c0608df254c35b58d55b545b8784c20dc
Yoga Raditya <144443155+yogarn@users.noreply.github.com>
2023-09-11 13:41:48 +0700

    add multiple document support

--- sign.py
+++ sign.py
@@ -1,20 +1,24 @@
 from fitz import fitz, Rect
 
 def start(input, output, signature, name):
+    # split user input using comma
     input = input.split(',')
     output = output.split(',')
     print(input[0])
-    for i in range(len(input)):
+    for i in range(len(input)): # loop and open each document
+        # remove any whitespace
         input_path = input[i].strip()
         output_path = output[i].strip()
-        doc = fitz.open(input_path)
-        img = open(signature, "rb").read()
-        for page in doc:
-            text_instances = page.search_for(name)
-            for inst in text_instances:
-                inst = Rect(inst.x0-20, inst.y0-80, inst.x1, inst.y1-10) # adjust this value if your signature looks awful
+        doc = fitz.open(input_path) # open document
+        img = open(signature, "rb").read() # load signature image
+        for page in doc: # loop in case of multiple page document
+            text_instances = page.search_for(name) # search name or keywords to be signed
+            for inst in text_instances: # loop in case found more than one name or keywords
+                # get name or keywords coordinate and adjust it
+                # change this value if your signature looks awful
+                inst = Rect(inst.x0-20, inst.y0-80, inst.x1, inst.y1-10)
                 if not page.is_wrapped:
                     page.wrap_contents()
-                page.insert_image(inst, stream=img)
-                print(inst)
-        doc.save(output_path)
+                page.insert_image(inst, stream=img) # insert signature image
+        doc.save(output_path) # save output document
+        print("Succesfully signing " + output_path)
